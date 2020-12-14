2d2209e12fe3f5d1987bafd1047e8c8420eb22cb
akmish <mail2akshatmishra@gmail.com>
2020-12-15 00:54:37 +0530

    implemented parallel execution

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -3,11 +3,10 @@ require('dotenv').config();
 module.exports = {
         "src_folders" : ["tests"],
         "page_objects_path": ["page-objects"],
-      
-        "webdriver" : {
-          "start_process": true,
-          "server_path": "node_modules/.bin/chromedriver",
-          "port": 9515
+
+        "test_workers": {
+          "enabled": false,
+          "workers": "auto"
         },
       
         "test_settings" : {
@@ -18,12 +17,36 @@ module.exports = {
               'on_error': true,
               'path': 'tests_output/screenshots'
             },
+            "webdriver" : {
+              "start_process": true,
+              "server_path": "node_modules/.bin/chromedriver",
+              "port": 9515,
+            },
             "desiredCapabilities": {
-              "browserName": "chrome",
-              'chromeOptions': {
-                "args": ["--headless"]
-              }
+              "browserName": "chrome"
+              // 'chromeOptions': {
+              //  // "args": ["--headless"]
+              // }
             }
-          }
+          },
+          "firefox" : {
+            'screenshots': {
+              'enabled': true,
+              'on_failure': true,
+              'on_error': true,
+              'path': 'tests_output/screenshots'
+            },
+            "webdriver" : {
+              "start_process": true,
+              "server_path": "./node_modules/.bin/geckodriver",
+              "port": 9516
+            },
+            "desiredCapabilities": {
+              "browserName": "firefox",
+              "marionette": true,
+              "javascriptEnabled": true,
+              "acceptSslCerts": true
+            }
+          },
         }
       };
