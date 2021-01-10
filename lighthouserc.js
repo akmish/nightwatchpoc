c85720e6ff34ce7b5a347764af99c62a12b87132
akmish <mail2akshatmishra@gmail.com>
2021-01-10 20:28:39 +0530

    integrated lighthouse ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,26 @@
 module.exports = {
   ci: {
     collect: {
-      url:
-        "https://mediacorp%23%40cms:mediacorp%40%23%211%23@mediacorpcorestg.prod.acquia-sites.com/cna-homepage",
+      url: [
+        // "https://www.srijan.net/",
+        // "https://www.srijan.net/digital-consulting-services",
+        // "https://www.srijan.net/product-engineering-services#contentSticky",
+        // "https://www.srijan.net/application-modernization-services",
+        // "https://www.srijan.net/knowledge",
+        // "https://www.srijan.net/studio",
+        // "https://www.srijan.net/case-study",
+        "https://www.google.com/",
+        "https://www.google.com/advanced_search",
+        "https://duckduckgo.com/",
+      ],
       numberOfRuns: 1,
+      assert: {
+        //preset: "lighthouse:recommended",
+        assertions: {
+          "categories:performance": ["error", { minScore: 0.25 }],
+          "categories:accessibility": ["warn", { minScore: 0.55 }],
+        },
+      },
     },
     upload: {
       target: "temporary-public-storage",
